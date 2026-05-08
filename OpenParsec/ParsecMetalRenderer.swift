2dcfb3c3cbbe7e3517157a383089374006508ddc
SandiyosDev <25407129+SandiyosDev@users.noreply.github.com>
2026-05-08 16:07:38 -0500

    metal renderer and view controller

--- OpenParsec/ParsecMetalRenderer.swift
+++ OpenParsec/ParsecMetalRenderer.swift
@@ -1,43 +1,43 @@
-/*import MetalKit
+import MetalKit
 import ParsecSDK
 
-class ParsecMetalRenderer:NSObject, MTKViewDelegate
-{
-	var parent:ParsecMetalViewController
-	var onBeforeRender:() -> Void
-	var metalDevice:MTLDevice!
-	var metalCommandQueue:MTLCommandQueue!
-	var metalTexture:MTLTexture!
-	var metalTexturePtr: UnsafeMutableRawPointer?
-	
-	var lastWidth:CGFloat = 1.0
+class ParsecMetalRenderer: NSObject, MTKViewDelegate {
+	let commandQueue: MTLCommandQueue
+	let updateImage: () -> Void
+	var lastWidth: CGFloat = 1.0
+	var renderTexture: MTLTexture?
 
-	init(_ parent:ParsecMetalViewController, _ beforeRender:@escaping () -> Void)
-	{
-		self.parent = parent;
-		onBeforeRender = beforeRender
-		if let metalDevice = MTLCreateSystemDefaultDevice()
-		{
-			self.metalDevice = metalDevice
-		}
-		self.metalCommandQueue = metalDevice.makeCommandQueue()
-		metalTexture = metalDevice.makeTexture(descriptor:MTLTextureDescriptor())
-		metalTexturePtr = createTextureRef(&metalTexture)
-		
+	init(_ view: MTKView, _ commandQueue: MTLCommandQueue, _ updateImage: @escaping () -> Void) {
+		self.commandQueue = commandQueue
+		self.updateImage = updateImage
 		super.init()
 	}
-	
-	func mtkView(_ view:MTKView, drawableSizeWillChange size: CGSize) { }
-	
-	func draw(in view:MTKView)
-	{
-		onBeforeRender()
-		let deltaWidth: CGFloat = view.frame.size.width - lastWidth
-		if deltaWidth > 0.1 || deltaWidth < -0.1
-		{
+
+	func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+	}
+
+	func draw(in view: MTKView) {
+		guard let drawable = view.currentDrawable else { return }
+		if CParsec.getStatus() != PARSEC_OK { return }
+
+		let deltaWidth = view.frame.size.width - lastWidth
+		if deltaWidth > 0.1 || deltaWidth < -0.1 {
 			CParsec.setFrame(view.frame.size.width, view.frame.size.height, view.contentScaleFactor)
 			lastWidth = view.frame.size.width
 		}
-		CParsec.renderMetalFrame(&metalCommandQueue, &metalTexturePtr)
+
+		let fps = SettingsHandler.preferredFramesPerSecond == 0
+			? UIScreen.main.maximumFramesPerSecond
+			: SettingsHandler.preferredFramesPerSecond
+		let timeout = UInt32(max(1000 / fps, 8))
+
+		renderTexture = drawable.texture
+		let _ = CParsec.renderMetalFrame(commandQueue, &renderTexture, timeout: timeout)
+
+		guard let cb = commandQueue.makeCommandBuffer() else { return }
+		cb.present(drawable)
+		cb.commit()
+
+		updateImage()
 	}
-}*/
+}
--- OpenParsec/ParsecMetalViewController.swift
+++ OpenParsec/ParsecMetalViewController.swift
@@ -1,31 +1,51 @@
-/*import SwiftUI
+import UIKit
 import MetalKit
 
-struct ParsecMetalViewController: UIViewRepresentable
-{
-	let onBeforeRender:() -> Void
-	
-	func makeCoordinator() -> ParsecMetalRenderer
-	{
-		ParsecMetalRenderer(self, onBeforeRender)
+class ParsecMetalViewController: ParsecPlayground {
+
+	var metalView: MTKView!
+	var metalDevice: MTLDevice!
+	var commandQueue: MTLCommandQueue!
+	var metalRenderer: ParsecMetalRenderer!
+	let updateImage: () -> Void
+
+	let viewController: UIViewController
+
+	required init(viewController: UIViewController, updateImage: @escaping () -> Void) {
+		self.viewController = viewController
+		self.updateImage = updateImage
 	}
-	
-	func makeUIView(context: UIViewRepresentableContext<ParsecMetalViewController>) -> MTKView
-	{
-		let metalView = MTKView()
-		metalView.delegate = context.coordinator
-		metalView.preferredFramesPerSecond = 60
-		metalView.enableSetNeedsDisplay = true
-		
-		if let metalDevice = MTLCreateSystemDefaultDevice()
-		{
-			metalView.device = metalDevice
-		}
-		
+
+	func viewDidLoad() {
+		guard let device = MTLCreateSystemDefaultDevice() else { return }
+		metalDevice = device
+		commandQueue = device.makeCommandQueue()
+
+		metalView = MTKView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+		metalView.device = device
+		metalView.colorPixelFormat = .bgra8Unorm
 		metalView.framebufferOnly = false
-		metalView.drawableSize = metalView.frame.size
-		return metalView
+		metalView.isPaused = false
+		metalView.enableSetNeedsDisplay = false
+
+		let fps = SettingsHandler.preferredFramesPerSecond
+		if fps == 0 {
+			metalView.preferredFramesPerSecond = UIScreen.main.maximumFramesPerSecond
+		} else {
+			metalView.preferredFramesPerSecond = fps
+		}
+
+		metalRenderer = ParsecMetalRenderer(metalView, commandQueue, updateImage)
+		metalView.delegate = metalRenderer
+
+		viewController.view.addSubview(metalView)
 	}
-	
-	func updateUIView(_ uiView:MTKView, context: UIViewRepresentableContext<ParsecMetalViewController>) { }
-}*/
+
+	func cleanUp() {
+		metalView?.removeFromSuperview()
+	}
+
+	func updateSize(width: CGFloat, height: CGFloat) {
+		metalView.frame.size = CGSize(width: width, height: height)
+	}
+}
